e3957372d1bb3778b20e335ca6de28aed95ed518
Pearson <pearseb007@gmail.com>
2015-02-06 16:12:50 -0700

    Added modules for API calls.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,43 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', 'Restangular', function ($routeProvider, Restangular) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+        Restangular.setBaseUrl('http://www.googleapis.com');
+    }])
 
-.controller('View1Ctrl', [function() {
+    .factory('Query', function($http, Restangular, $q) {
+        var query = [];
 
-}]);
+        query.repQuery = function (queryString) {
+            var deferred = $q.defer();
+            Restangular.one('civicinfo/v2/representatives?key=AIzaSyD-HPE_alWclw0dk45SVc87VhH1FJT-j5o&' + queryString)
+                .get()
+                .then(function (repInfo) {
+                    console.log(repInfo);
+                    deferred.resolve(repInfo);
+                }, function (error) {
+                    alert("error " + error);
+                    deferred.reject(error)
+                });
+
+            return deferred.promise;
+
+        return query;
+        }
+    })
+
+    //.factory('TweetQuery', function($http, SwitchRestangular, $q, $scope) {
+    //
+    //})
+
+    .controller('View1Ctrl', [function (Query) {
+        var query = Query.repQuery("variable").then(function (data){
+            $scope.reps = data;
+        }, function(error){
+            code
+        })
+    }]);
