54f99c89bfeaaa9d0e897a18b5f9e98cd60e972f
Joshua Hancock <jhancock1229@gmail.com>
2015-02-11 10:59:49 -0700

    Directives

--- app/sections/section-two/section-two.js
+++ app/sections/section-two/section-two.js
@@ -1,11 +1,11 @@
 angular.module('myApp.sections')
 
     .controller('sectionTwoCtrl', ['$scope', function ($scope) {
-        console.log("LOADED SECTION TWO CTRL");
-        $scope.username = "Orrin Hatch"
-        $scope.vote = "Yes";
-        $scope.voting = function () {
-            $scope.voted = $scope.senatorname + ":" + $scope.vote;
-        };
+        $scope.name = "Peter Anderson";
+        $scope.party = "?";
+        $scope.tweet = "Coding is awesome!";
+        $scope.vote = "Did Not Vote";
+        $scope.bill = "H.B. 100";
+        $scope.comment = "I don't work in politics. I honestly don't know why I am here.";
 
     }]);
